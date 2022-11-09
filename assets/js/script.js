fa10adb5ca80a75ec936788343fa4e4bf03c2ea2
Bfunk54 <ben_fein@icloud.com>
2022-11-09 00:28:29 -0500

    added a statement to check if the password length input is between 8 and 128 chars

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,6 @@ let lowercaseLetters = ['a', 'b' , 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
 let uppercaseLetters = ['A', 'B' , 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', '<', '>', ',', '.', '?', '/', '|', '`', '~'];
-let letterLength
 
 // Write password to the #password input
 function writePassword() {
@@ -22,8 +21,14 @@ function writePassword() {
 
 }
 
+// Function checking for which options the user has selected
 function passwordOptions() {
 	let pass = [];
+	if (passLength < 8 || passLength > 128) {
+		alert('Please try for a password between 8 and 128 characters');
+		return;
+	}
+	else {
 	switch (true) {
 		case passLower.checked && passUpper.checked && passNumber.checked && passSymbols.checked: pass = lowercaseLetters.concat(uppercaseLetters, numbers, specialCharacters); createPassword(pass);
 			break;
@@ -56,6 +61,7 @@ function passwordOptions() {
 		case passSymbols.checked: pass = specialCharacters; createPassword(pass);
 			break;
 		default: alert('Please select at least one character type.');
+		}
 	}
 }
 
