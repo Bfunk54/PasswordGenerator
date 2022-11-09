302d4aa635529b13f3d0e11f53fb16333beef255
Bfunk54 <ben_fein@icloud.com>
2022-11-09 00:34:45 -0500

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ let passNumber = document.getElementById('numbers');
 let passSymbols = document.getElementById('symbols');
 let thePass = document.getElementById('password');
 
+// Added global variables for the different character types
 let lowercaseLetters = ['a', 'b' , 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let uppercaseLetters = ['A', 'B' , 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -24,10 +25,12 @@ function writePassword() {
 // Function checking for which options the user has selected
 function passwordOptions() {
 	let pass = [];
+	// Check if the password is less than 8 or more than 128 characters
 	if (passLength < 8 || passLength > 128) {
 		alert('Please try for a password between 8 and 128 characters');
 		return;
 	}
+	// As long as it's not, run the switch to figure out which options the user has selected
 	else {
 	switch (true) {
 		case passLower.checked && passUpper.checked && passNumber.checked && passSymbols.checked: pass = lowercaseLetters.concat(uppercaseLetters, numbers, specialCharacters); createPassword(pass);
@@ -65,7 +68,8 @@ function passwordOptions() {
 	}
 }
 
-function createPassword() {
+// Use the selected options to create the password
+function createPassword(pass) {
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
