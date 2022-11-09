f13e9a511777d1c1c987771642e3b8b5021df9ab
Bfunk54 <ben_fein@icloud.com>
2022-11-08 23:20:25 -0500

    added arrays with all different character cases in the global scope

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
+let lowercaseLetters = ['a', 'b' , 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let uppercaseLetters = ['A', 'B' , 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', '<', '>', ',', '.', '?', '/', '|', '`', '~'];
+
 // Write password to the #password input
 function writePassword() {
   let password = generatePassword();
@@ -8,6 +13,9 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function createPassword() {
 }
 
 // Add event listener to generate button
